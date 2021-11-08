2421ba317ea0af1a98d17c23f23d84b947a9ad6a
Vish O <v.opatha@gmail.com>
2021-11-09 09:08:24 +1030

    Add component stack in App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import React from 'react';
+import styled from 'styled-components';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import MainContent from './components/MainContent';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Container>
+        <Navbar />
+        <MainContent />
+      </Container>
+
+      <Footer />
+    </>
   );
 }
+
+const Container = styled.div`
+  // background: #fbf7ef;
+  // height: 100vh;
+`;
 
 export default App;
