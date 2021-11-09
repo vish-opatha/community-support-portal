c6496250d097ed8d002e6bfe21ae206086314abc
Vish O <v.opatha@gmail.com>
2021-11-09 13:33:21 +1030

    Create Agency Services object

---
+++ src/components/servicesAvailable.js
@@ -0,0 +1,76 @@
+const agencyServices = [
+  {
+    servicesIdentifier: 'PF',
+    description: 'Provide Food',
+  },
+  {
+    servicesIdentifier: 'PF',
+    description: 'Provide Clothes',
+  },
+  {
+    servicesIdentifier: 'PF',
+    description: 'Provide support for furniture and bedding',
+  },
+  {
+    servicesIdentifier: 'PF',
+    description: 'Provide Food',
+  },
+  {
+    servicesIdentifier: 'PF',
+    description: 'Provide support for recover',
+  },
+  {
+    servicesIdentifier: 'PF',
+    description: 'Provide Food',
+  },
+  {
+    servicesIdentifier: 'PF',
+    description: 'Provide new toys for Christmas',
+  },
+  {
+    servicesIdentifier: 'PF',
+    description: 'Provide Christmas Lunch',
+  },
+  {
+    servicesIdentifier: 'PF',
+    description: 'Provide support to write your last will',
+  },
+  {
+    servicesIdentifier: 'PF',
+    description: 'Provide Food',
+  },
+  {
+    servicesIdentifier: 'PF',
+    description: 'Accept all donations to the thrift shop',
+  },
+  {
+    servicesIdentifier: 'PF',
+    description: 'Accept limited donations for thrift shop',
+  },
+  {
+    servicesIdentifier: 'PF',
+    description: 'Provide financial couselling',
+  },
+  {
+    servicesIdentifier: 'PF',
+    description: 'Provide support to find a job',
+  },
+  {
+    servicesIdentifier: 'PF',
+    description: 'Provide vouchers for supermarkets and Foodbank',
+  },
+  {
+    servicesIdentifier: 'PF',
+    description: 'Deliver food to your doorstep',
+  },
+  {
+    servicesIdentifier: 'PF',
+    description: 'Provide housing advice',
+  },
+  {
+    servicesIdentifier: 'PF',
+    description: 'Provide Food',
+  },
+];
+
+export default agencyServices;
