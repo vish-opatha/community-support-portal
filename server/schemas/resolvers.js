e65ab43f57e474f02f0cba08129844febb7f59fe
Vish O <v.opatha@gmail.com>
2021-11-10 13:25:29 +1030

    Add resolvers and typeDefs for ServiceCategory

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,4 +1,4 @@
-const { User } = require('../models');
+const { User, ServiceCategory } = require('../models');
 
 // Create the functions that fulfill the queries defined in `typeDefs.js`
 const resolvers = {
@@ -6,8 +6,11 @@ const resolvers = {
     users: async () => {
       // Get and return all documents from the classes collection
       return await User.find({});
-    }
-  }
+    },
+    serviceCategories: async () => {
+      return await ServiceCategory.find({});
+    },
+  },
 };
 
 module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -6,16 +6,23 @@ const typeDefs = gql`
     firstName: String
     lastName: String
     organisation: String
-    location : String
+    location: String
     email: String
     telephone: String
     orgWebsite: String
     password: String
   }
 
+  type ServiceCategory {
+    _id: ID
+    description: String
+    customerDescription: String
+  }
+
   # Define which queries the front end is allowed to make and what data is returned
   type Query {
     users: [User]
+    serviceCategories: [ServiceCategory]
   }
 `;
 
