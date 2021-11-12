6ce3444a1af3fa86dc4b542945d0437dc191cf6a
Vish O <v.opatha@gmail.com>
2021-11-12 19:44:20 +1030

    Update google fonts

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-// import './App.css';
+import './App.css';
 import React from 'react';
 import styled from 'styled-components';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
