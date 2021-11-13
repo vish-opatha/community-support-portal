95f9197fa714458a4cd2680277b15f952db0d4b4
Vish O <v.opatha@gmail.com>
2021-11-13 17:29:20 +1030

    Add another search option

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -7,13 +7,13 @@ const Search = () => {
         <h3 className="flow-text" style={{ fontWeight: 600 }}>
           Search for the services
         </h3>
-        <div className="col s12">
+        <div className="col s12 m8">
           <label style={{ fontWeight: 600 }}>
             Search for the support you want
           </label>
           <select
             id="category"
-            class="browser-default"
+            className="browser-default"
             // onfocus="displayOptions()"
             // onchange="displayResults()"
           >
@@ -21,6 +21,12 @@ const Search = () => {
               I need to..
             </option>
           </select>
+        </div>
+        <div className="col s12 m4">
+          <label for="suburb" style={{ fontWeight: 600 }}>
+            Suburb
+          </label>
+          <input id="suburb" type="text" />
         </div>
       </div>
     </div>
