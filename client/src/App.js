b13f1f8a57c21c9154ef63338b14fecbe3a51c44
Vish O <v.opatha@gmail.com>
2021-11-13 11:33:48 +1030

    Update App.js with Footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import React from 'react';
 import Header from './components/Header';
+import Footer from './components/Footer';
 // import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 // import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 // import AgencyLogin from './components/styles/AgencyLoginForm';
@@ -18,6 +19,7 @@ function App() {
   return (
     <>
       <Header />
+      <Footer />
     </>
   );
   // <ApolloProvider client={client}>
