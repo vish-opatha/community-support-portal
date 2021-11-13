008b184054574d74ddc0a91b1ffcfc3fd7071fd0
Vish O <v.opatha@gmail.com>
2021-11-13 10:56:16 +1030

    Update header components

---
+++ client/src/components/Header.js
@@ -0,0 +1,57 @@
+import React from 'react';
+
+const Header = () => {
+  //   const [isOpen, setIsOpen] = useState(false);
+  return (
+    <>
+      <nav>
+        <div className="red accent-2 nav-wrapper">
+          <a href="/" className="brand-logo">
+            Relief Support SA
+          </a>
+          {/* <a href="#" data-target="mobile-demo" className="sidenav-trigger">
+            <i class="material-icons">menu</i>
+          </a> */}
+          <ul class="right hide-on-med-and-down">
+            <li>
+              <a href="/agencysignup">AgencySignup</a>
+            </li>
+            <li>
+              <a href="/agencylogin">Agency Login</a>
+            </li>
+            <li>
+              <a href="/search">Search</a>
+            </li>
+            <li>
+              <a href="/logout" className="button is-text" id="logoutBtn">
+                Logout
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+
+      <ul class="sidenav" id="mobile-demo">
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/search">Search Items</a>
+        </li>
+        <li>
+          <a href="/userProfile">My Profile</a>
+        </li>
+        {/* <li>
+          <a className="button is-text" id="mobilelogoutBtn">
+            Logout
+          </a>
+        </li> */}
+        <li>
+          <a href="/login">Login</a>
+        </li>
+      </ul>
+    </>
+  );
+};
+
+export default Header;
