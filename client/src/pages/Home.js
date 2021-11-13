bb25adc8d3ce13bb8829c445cd0396fd1944b13b
Vish O <v.opatha@gmail.com>
2021-11-13 17:29:59 +1030

    Add Home Page

---
+++ client/src/pages/Home.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <>
+      <img src="/images/cover.jpg" alt="Cover" className="responsive-img" />
+      <div className="row center-align">
+        <div className="col s12">
+          <h3 className="flow-text">
+            We are here provide the information, to get the support you want.
+            You are valued, loved and blessed!
+          </h3>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
