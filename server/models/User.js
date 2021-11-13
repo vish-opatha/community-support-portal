bc484f6302bd0049f9830f03c3cfac380c21c640
Vish O <v.opatha@gmail.com>
2021-11-13 17:49:26 +1030

    Update Server side User model for authentication

--- server/models/User.js
+++ server/models/User.js
@@ -1,63 +1,67 @@
 const { Schema, model } = require('mongoose');
-const serviceProvided = require('./ServiceProvided');
+const bcrypt = require('bcrypt');
 
-const userSchema = new Schema(
-  {
-    title:{
-      type:String,
-      required: true,
-      trim: true,
-    },
-    firstName: {
-      type: String,
-      required: true,
-      trim: true
-    },
-    lastName: {
-      type: String,
-      required: true,
-      trim: true
-    },
-    organisation: {
-      type: String,
-      required: true,
-      trim: true
-    },
-    location: {
-      type: String,
-      required: true,
-      trim: true
-    },
-    email: {
-      type: String,
-      required: true,
-      trim: true,
-      unique: true,
-      match: [/.+@.+\..+/, 'Please enter a valid email address'],
-    },
-    telephone: {
-      type: String,
-      required: true,
-      trim: true
-    },
-    orgWebsite: {
-      type: String,
-      trim: true
-    },
-    password: {
-      type: String,
-      required: true,
-    },
-    // services:[serviceProvided],
-    // services: [
-    //   {
-    //     type: Schema.Types.ObjectId,
-    //     ref: 'serviceProvided'
-    //   }
-    // ]
+const userSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  firstName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  organisation: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  location: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    unique: true,
+    match: [/.+@.+\..+/, 'Please enter a valid email address'],
+  },
+  telephone: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  orgWebsite: {
+    type: String,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+userSchema.pre('save', async function (next) {
+  if (this.isNew || this.isModified('password')) {
+    const saltRounds = 12;
+    this.password = await bcrypt.hash(this.password, saltRounds);
   }
-);
+
+  next();
+});
+
+userSchema.methods.isCorrectPassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
 
 const User = model('User', userSchema);
 
-module.exports = User;
+module.exports = { User, userSchema };
