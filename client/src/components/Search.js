8082a8a5dd9b3b05adc6f1329b231436c675739e
Vish O <v.opatha@gmail.com>
2021-11-13 16:53:03 +1030

    Update Search component with styles

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -4,14 +4,18 @@ const Search = () => {
   return (
     <div className="container center">
       <div className="row">
-        <h3 className="flow-text">Search for the services</h3>
+        <h3 className="flow-text" style={{ fontWeight: 600 }}>
+          Search for the services
+        </h3>
         <div className="col s12">
-          <label>Search by Category</label>
+          <label style={{ fontWeight: 600 }}>
+            Search for the support you want
+          </label>
           <select
             id="category"
             class="browser-default"
-            onfocus="displayOptions()"
-            onchange="displayResults()"
+            // onfocus="displayOptions()"
+            // onchange="displayResults()"
           >
             <option value="" disabled selected>
               I need to..
