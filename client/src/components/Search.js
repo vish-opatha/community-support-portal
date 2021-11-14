b237783f69a40b9d63f96bc2d974528612638314
Vish O <v.opatha@gmail.com>
2021-11-15 08:05:34 +1030

    Add Search component

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -1,7 +1,48 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ServiceOption from './ServicesOption';
-
+const agencyServices = [
+  {
+      description: "Provide Food",
+      label: "I want help with food",
+      value:1
+    },
+    {
+      description: "Provide Bustickets",
+      label: "I want help with Bustickets",
+      value:2
+    },
+    {
+      description: "Provide Clothing",
+      label: "I want help with Clothing",
+      value:3
+    },
+    {
+      description: "Provide Furniture",
+      label: "I want help with essential food",
+      value:4
+    },
+    {
+      description: "Provide help for domestic violence victims",
+      label: "I want help with domestic violence",
+      value:5
+    }
+  ];
 const Search = () => {
+
+  // const [dropDownValue,setValue]=useState({categoryId:""})
+  // const dropDownChangeHandler = e =>
+  // {
+  //   setValue(e.key);
+  //   console.log(dropDownValue);
+
+  // }
+
+ const [result, selectedValue] = useState(agencyServices.label);
+ const selectHandle = e =>
+ {
+    selectedValue(e.label);
+ }
+
   return (
     <div className="container center">
       <div className="row">
@@ -12,12 +53,8 @@ const Search = () => {
           <label style={{ fontWeight: 600 }}>
               Search for the support you want
           </label>
-          <select id="category" className="browser-default"> 
-            {/* // onfocus="displayOptions()"
-            // onchange="displayResults()" */}
-              <option value="" disabled selected>I need </option>
-              <ServiceOption/>
-          </select>
+          <ServiceOption onChange={selectHandle} />
+          <div>{result}</div>
         </div>
         <div className="col s12 m4">
           <label for="suburb" style={{ fontWeight: 600 }}>
