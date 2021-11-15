d8d691cec5628dbdf0cb7596355646dacbd79fc0
Vish O <v.opatha@gmail.com>
2021-11-15 21:06:37 +1030

    Update layout changes in Home

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,8 +3,8 @@ import React from 'react';
 const Home = () => {
   return (
     <>
+    <div className="row center-align">
       <img src="/images/cover.jpg" alt="Cover" className="responsive-img" />
-      <div className="row center-align">
         <div className="col s12">
           <h3 className="flow-text" style={{fontWeight:600}}>
             We are here provide the information, to get the support you want.
