2098b4ebde6a1a7f74de27891762f256e72fa0eb
Vish O <v.opatha@gmail.com>
2021-11-15 15:03:28 +1030

    Update Search component card and icons

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import { useQuery } from '@apollo/client';
 import {SEARCH_BY_LOCATION} from '../utils/queries';
+import { ImLocation} from 'react-icons/im'
 
 const Search = () => {
    const [searchData, setSearchData] =useState([]);
@@ -21,7 +22,7 @@ const Search = () => {
    
     <div className="container center">
         <div className="row center">
-            <h3 className="flow-text">You can search the services from here</h3>
+            <h3 className="flow-text" style={{fontWeight:600}}>You can search the services from here</h3>
             <div className="col s12 m12">
               <label>Search by Category</label>
                 <select id="category" className="browser-default">
@@ -41,15 +42,23 @@ const Search = () => {
         {data && data.servicesByLocation.map((item)=> {
           return(
             <>
-                <div className="col s12 m12">
+                <div className="col s12 m6">
                   <div className="card white darken-1">
                     <div className="card-content black-text">
-                      <span className="card-title">{item.modeOfCommunication}</span>
-                          <p>{item.location} {item.eligibility}</p>
-                          <p>Things for you to consider</p>
-                          <br/>
+                      <span className="card-title" style={{fontWeight:'bold',color:"#004d40"}}>{item.description}</span>
+                      <span style={{fontWeight:'bold', fontSize:15}}>{item.organisation}</span>
+                      <p style={{fontWeight:'bold'}}>{`Description`}</p>{item.eligibility}
+                      <p></p>
+                      <p></p>
+                      <p style={{fontWeight:'bold'}}>{`Opened Days`}</p>{item.openedDays}
+                      <p style={{fontWeight:'bold'}}>{`Opened Hours`}</p>{item.openedHours}
+                      
+                      <div style={{height:20}}></div>
+
+                      <p style={{fontWeight:'bold'}}> Method of Communication</p>
+                      <p style={{fontWeight:'bold'}}> {item.modeOfCommunication}</p>
+                      <p style={{fontWeight:'bold'}}><ImLocation/>{`   `}{item.location}</p>                         
                           
-                          <p>Description{` `} {item.openedDays}{item.eligibility}</p>
                     </div>
                   </div>
                 </div>
